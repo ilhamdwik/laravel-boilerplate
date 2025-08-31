893e86241cfef7fb01a915d6a837c663932fbca9
ilhaskam <ilhaskam@gmail.com>
2025-08-31 18:24:36 +0700

    jumlah virtual user 20 & durasi test 30s

--- k6/script.js
+++ k6/script.js
@@ -1,3 +1,24 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export const options = {
+  vus: 20,          // jumlah virtual user
+  duration: '30s',  // durasi test
+
+  thresholds: {
+    http_req_failed: ['rate<0.01'],   // gagal kalau >1% request error
+    http_req_duration: ['p(95)<1000'], // gagal kalau 95% request >1s
+  },
+};
+
+export default function () {
+  let res = http.get('http://10.10.3.40');
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+}
+
+
 // import http from 'k6/http';
 // import { check, sleep } from 'k6';
 
@@ -15,19 +36,19 @@
 // }
 
 
-import http from 'k6/http';
-import { check } from 'k6';
+// import http from 'k6/http';
+// import { check } from 'k6';
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.01'], // gagal kalau >1% request error
-    http_req_duration: ['p(95)<1000'], // gagal kalau 95% request >1s
-  },
-};
+// export const options = {
+//   thresholds: {
+//     http_req_failed: ['rate<0.01'], // gagal kalau >1% request error
+//     http_req_duration: ['p(95)<1000'], // gagal kalau 95% request >1s
+//   },
+// };
 
-export default function () {
-  let res = http.get('http://10.10.3.40');
-  check(res, {
-    'status is 200': (r) => r.status === 200,
-  });
-}
+// export default function () {
+//   let res = http.get('http://10.10.3.40');
+//   check(res, {
+//     'status is 200': (r) => r.status === 200,
+//   });
+// }
